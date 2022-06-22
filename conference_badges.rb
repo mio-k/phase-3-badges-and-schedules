5bdf0ad53dbe0173205b536004672311ffcc7b70
mio kataoka <mio.kanna@gmail.com>
2022-06-21 19:36:28 -0700

    commit tree d75cccbbc75b7d84b45a812f0e786e57f0248598

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map do |person|
+        badge_maker(person)
+    end
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+end 
+
+def printer(arr)
+    batch_badge_creator(arr).each do |badge|
+        puts badge
+    end
+    assign_rooms(arr).each do |assignment|
+        puts assignment 
+    end
+end
+
